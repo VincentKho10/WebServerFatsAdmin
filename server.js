28c1cbb6412b697c05ac430b1b5aaf09aa4da534
VincentKho10 <vincentkho10@gmail.com>
2022-04-20 10:21:01 +0700

    test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express= require('express')
 const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
 const db = mongoose.connection
@@ -11,22 +12,23 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(express.json())
 
 const dosenRouter = require('./routes/dosens')
-app.use('/dosen', dosenRouter)
+app.use('/dosen', cors(), dosenRouter)
 
 const mahasiswaRouter = require('./routes/mahasiswas')
-app.use('/mahasiswa', mahasiswaRouter)
+app.use('/mahasiswa', cors(), mahasiswaRouter)
 
 const userRouter = require('./routes/mahasiswa_users')
-app.use('/user', userRouter)
+app.use('/user', cors(), userRouter)
 
 const jadwalRouter = require('./routes/jadwals')
-app.use('/jadwal', jadwalRouter)
+app.use('/jadwal', cors(), jadwalRouter)
 
 const kehadiranRouter = require('./routes/kehadirans')
-app.use('/kehadiran', kehadiranRouter)
+app.use('/kehadiran', cors(), kehadiranRouter)
 
 app.listen(process.env.PORT || 1337, () => console.log('Server Started'))
